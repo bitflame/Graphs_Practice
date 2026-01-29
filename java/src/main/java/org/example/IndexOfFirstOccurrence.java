639a122af052b3bf6855dc72991f7804883f3c11
shahin <shahinnumone@outlook.com>
2026-01-28 21:00:22 -0500

    Added problem 28 - added Sidgewick's brute force code to see how I did.

--- java/src/main/java/org/example/IndexOfFirstOccurrence.java
+++ java/src/main/java/org/example/IndexOfFirstOccurrence.java
@@ -24,21 +24,41 @@ public class IndexOfFirstOccurrence {
         return pattern == n ? text - n : -1;
     }
 
+    public int sidgwickBruteForce(String haystack, String needle) {
+        int j, M = needle.length();
+        int i, N = haystack.length();
+        for (i = 0, j = 0; i < N && j < M; i++) {
+            if (haystack.charAt(i) == needle.charAt(j)) j++;
+            else {
+                i -= j;
+                j = 0;
+            }
+
+        }
+        if (j == M) return i - M;
+        else return -1;
+    }
+
     public static void main(String[] args) {
         String hay = "sadbutsad";
         String ndl = "sad";
         IndexOfFirstOccurrence io = new IndexOfFirstOccurrence();
         System.out.printf("Test 1 - expected output: 0, actual: %d\n", io.strStr(hay, ndl));
+        System.out.printf("Test 1 - Sidgwick expected output: 0, actual: %d\n", io.sidgwickBruteForce(hay, ndl));
         hay = "leetcode";
         ndl = "leeto";
         System.out.printf("Test 2 - expected output: -1, actual: %d\n", io.strStr(hay, ndl));
+        System.out.printf("Test 2 - Sidgwick expected output: -1, actual: %d\n", io.sidgwickBruteForce(hay, ndl));
         hay = "mississippi";
         ndl = "issip";
         System.out.printf("Test 3 - expected output: 4, actual: %d\n", io.strStr(hay, ndl));
+        System.out.printf("Test 3 - Sidgwick expected output: 4, actual: %d\n", io.sidgwickBruteForce(hay, ndl));
         ndl = "issipi"; // hay is the same for this test
         System.out.printf("Test 4 - expected output: -1, actual: %d\n", io.strStr(hay, ndl));
+        System.out.printf("Test 4 - Sidgwick expected output: -1, actual: %d\n", io.sidgwickBruteForce(hay, ndl));
         hay = "aaa";
         ndl = "aaaa";
         System.out.printf("Test 5 - expected output: -1, actual: %d\n", io.strStr(hay, ndl));
+        System.out.printf("Test 5 - Sidgwick expected output: -1, actual: %d\n", io.sidgwickBruteForce(hay, ndl));
     }
 }
