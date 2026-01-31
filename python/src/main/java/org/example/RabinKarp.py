9d9ad125134fc48b3d1c9739babb7f5c447a7c17
shahin <shahinnumone@outlook.com>
2026-01-31 13:22:30 -0500

    Added problem 28 - added RabinKarp but it does not work yet.

---
+++ python/src/main/java/org/example/RabinKarp.py
@@ -0,0 +1,52 @@
+
+class RabinKarp:
+    def strStr(self, haystack: str, needle: str) -> int:
+        Q = 2 ** 61 - 1
+        RM = 1
+        M = len(needle)
+        N = len(haystack)
+        R = 256
+        for i in range(1, M - 1):
+            RM = R * RM % Q
+        patHash = self.hash(needle, Q)
+        txtHash = self.hash(haystack, Q)
+        if patHash == txtHash: return 0
+        for i in range(M, N, 1):
+            txtHash = (txtHash + Q - RM * ord(haystack[i - M]) % Q) % Q
+            txtHash = (txtHash * R + ord(haystack[i])) % Q
+            if patHash == txtHash: return i - M + 1
+        return -1
+
+    def hash(self, needle: str, Q: int) -> int:
+        h = 0
+        R = 256
+        for j, c in enumerate(needle):
+            h = (R * h + ord(c)) % Q
+        return h
+
+
+rk = RabinKarp()
+haystack = "NEEDLE"
+needle = "NEEDLE"
+print(f'Test 1 - expected output: 0, actual: {rk.strStr(haystack, needle)}')
+haystack = "FINDINAHAYSTACKNEEDLEINA"
+needle = "NEEDLE"
+print(f'Test 2 - expected output: 15, actual: {rk.strStr(haystack, needle)}')
+haystack = "sadbustedsad"
+needle = "sad"
+print(f'Test 2 - expected output: 0, actual: {rk.strStr(haystack, needle)}')
+haystack = "leetcode"
+needle = "leeto"
+print(f'Test 4 - expected output: -1, actual: {rk.strStr(haystack, needle)}')
+haystack = "mississippi"
+needle = "issip"
+print(f'Test 5 - expected output: 4, actual: {rk.strStr(haystack, needle)}')
+haystack = "aaa"
+needle = "aaaa"
+print(f'Test 6 - expected output: -1, actual: {rk.strStr(haystack, needle)}')
+haystack = "mississippi"
+needle = "a"
+print(f'Test 7 - expected output: -1, actual: {rk.strStr(haystack, needle)}')
+haystack = "mississippi"
+needle = "sippia"
+print(f'Test 8 - expected output: -1, actual: {rk.strStr(haystack, needle)}')
