8bbbc6939aa87dc78147ccf4fc87e0b6c609605b
shahin <shahinnumone@outlook.com>
2026-02-01 12:08:08 -0500

    Added the start of TextJustify problem LeetCode problem 68

---
+++ java/src/main/java/org/example/TextJustif.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.List;
+
+public class TextJustif {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        StringBuilder sb = new StringBuilder();
+
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"This", "is", "an", "example", "of", "text", "justification"};
+
+    }
+}
