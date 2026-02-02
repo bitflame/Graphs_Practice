907d90e9b595e7f793a3fcc68b5fe19507ec618f
shahin <shahinnumone@outlook.com>
2026-02-01 20:33:54 -0500

    Added the start of TextJustify problem LeetCode problem 68. Going to change the java code to use the modulus to insert spaces into StringBuilder using the insert(). Learned the strategy from https://www.youtube.com/watch?v=KGfXrj7G0W0

--- java/src/main/java/org/example/TextJustif.java
+++ java/src/main/java/org/example/TextJustif.java
@@ -57,7 +57,62 @@ public class TextJustif {
         }
         return result;
     }
+    // todo - change the java code to use the modulus to insert spaces into StringBuilder using the insert()
+    public List<String> methodTwo(String[] words, int maxWidth) {
+        StringBuilder sb;
+        List<String> result = new ArrayList<>();
+        int end = 0, currWord = 0, wordsPerLine = 0;
+        String separator = " ";
+        while (currWord < words.length) {
+            separator = " ";
+            wordsPerLine = 0;
+            sb = new StringBuilder();
+            int remainder = 0;
+            while (currWord < words.length && end + words[currWord].length() <= maxWidth) {
+                end += words[currWord++].length() + 1;
+                wordsPerLine++;
+            }
+            int additionalSpaces = maxWidth - (end - 1);
 
+
+            if (currWord != words.length) {
+                if (wordsPerLine > 1) remainder = additionalSpaces % (wordsPerLine - 1);
+                if (additionalSpaces >= (wordsPerLine-1) && wordsPerLine > 1) {
+                    for (int i = 0; i <= additionalSpaces / (wordsPerLine - 1); i++) {
+                        sb.append(separator);
+                    }
+                    separator = sb.toString();
+                    sb = new StringBuilder();
+                }
+                int i = currWord - wordsPerLine;
+                for (; i < currWord - 1; i++) {
+                    sb.append(words[i]).append(separator);
+                    if (remainder > 0) {
+                        sb.append(" ");
+                        remainder--;
+                    }
+                }
+                sb.append(words[currWord - 1]);
+                if (wordsPerLine == 1) {
+                    while (sb.length() != maxWidth) {
+                        sb.append(" ");
+                    }
+                }
+            } else {
+                // if we are at last line...
+                separator = " ";
+                int i = currWord - wordsPerLine;
+                for (; i < currWord; i++) {
+                    sb.append(words[i]).append(separator);
+                }
+                while (sb.length() != maxWidth) sb.append(" ");
+            }
+            // how to deal with the last line?
+            result.add(sb.toString());
+            end = 0;
+        }
+        return result;
+    }
     public static void main(String[] args) {
         TextJustif tj = new TextJustif();
         String[] words = {"This", "is", "an", "example", "of", "text", "justification"};
---
+++ python/src/main/java/org/example/Justify.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Justify:
+    def fullJustify(self, words: List[str],maxWidth: int)-> List[str]:
+        res, line, width = [], [], 0
+        for w in words:
+            if width+len(w) + len(line)>maxWidth:
+                for i in range(maxWidth-width): line[i%(len(line)-1 or 1)]+=' '
+                res, line, width = res + [''.join(line)],[], 0
+            line += [w]
+            width += len(w)
+        return res + [' '.join(line).ljust(maxWidth)]
+j = Justify()
+words = ["This", "is", "an", "example", "of", "text", "justification"]
+maxWid = 16
+print(j.fullJustify(words,maxWid))
