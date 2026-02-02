c1f9307654a09d59396d6dc123f7018368aaeaf8
shahin <shahinnumone@outlook.com>
2026-02-02 11:23:00 -0500

    Added the start of TextJustify problem LeetCode problem 68. Going to change the java code to use the modulus to insert spaces into StringBuilder using the insert(). Learned the strategy from https://www.youtube.com/watch?v=KGfXrj7G0W0

--- java/src/main/java/org/example/TextJustif.java
+++ java/src/main/java/org/example/TextJustif.java
@@ -21,7 +21,7 @@ public class TextJustif {
             int additionalSpaces = maxWidth - (end - 1);
             if (currWord != words.length) {
                 if (wordsPerLine > 1) remainder = additionalSpaces % (wordsPerLine - 1);
-                if (additionalSpaces >= (wordsPerLine-1) && wordsPerLine > 1) {
+                if (additionalSpaces >= (wordsPerLine - 1) && wordsPerLine > 1) {
                     for (int i = 0; i <= additionalSpaces / (wordsPerLine - 1); i++) {
                         sb.append(separator);
                     }
@@ -57,76 +57,59 @@ public class TextJustif {
         }
         return result;
     }
-    // todo - change the java code to use the modulus to insert spaces into StringBuilder using the insert()
+
+
     public List<String> methodTwo(String[] words, int maxWidth) {
-        StringBuilder sb;
+        StringBuilder sb = new StringBuilder();
+        List<String> line = new ArrayList<>();
         List<String> result = new ArrayList<>();
-        int end = 0, currWord = 0, wordsPerLine = 0;
+        int totalChars = 0, currWordIndex = 0, wordsPerLine = 0, index = 0;
         String separator = " ";
-        while (currWord < words.length) {
-            separator = " ";
-            wordsPerLine = 0;
-            sb = new StringBuilder();
-            int remainder = 0;
-            while (currWord < words.length && end + words[currWord].length() <= maxWidth) {
-                end += words[currWord++].length() + 1;
-                wordsPerLine++;
-            }
-            int additionalSpaces = maxWidth - (end - 1);
-
-
-            if (currWord != words.length) {
-                if (wordsPerLine > 1) remainder = additionalSpaces % (wordsPerLine - 1);
-                if (additionalSpaces >= (wordsPerLine-1) && wordsPerLine > 1) {
-                    for (int i = 0; i <= additionalSpaces / (wordsPerLine - 1); i++) {
-                        sb.append(separator);
-                    }
-                    separator = sb.toString();
+        while (currWordIndex < words.length) {
+            if (totalChars + words[currWordIndex].length() > maxWidth) {
+                for (int i = 0; i < (maxWidth - totalChars); i++) {
+                    index = i % Math.max(line.size() - 1, 1);
+                    sb.append(line.remove(index));
+                    sb.append(separator);
+                    line.add(index, sb.toString());
                     sb = new StringBuilder();
                 }
-                int i = currWord - wordsPerLine;
-                for (; i < currWord - 1; i++) {
-                    sb.append(words[i]).append(separator);
-                    if (remainder > 0) {
-                        sb.append(" ");
-                        remainder--;
-                    }
-                }
-                sb.append(words[currWord - 1]);
-                if (wordsPerLine == 1) {
-                    while (sb.length() != maxWidth) {
-                        sb.append(" ");
-                    }
-                }
-            } else {
-                // if we are at last line...
-                separator = " ";
-                int i = currWord - wordsPerLine;
-                for (; i < currWord; i++) {
-                    sb.append(words[i]).append(separator);
-                }
-                while (sb.length() != maxWidth) sb.append(" ");
+                result.addAll(line);
+                line = new ArrayList<>();
+                totalChars = 0;
             }
-            // how to deal with the last line?
-            result.add(sb.toString());
-            end = 0;
+            if (currWordIndex == words.length - 1) {
+                sb.append(line.getLast());
+                for (int i = 0; i < maxWidth - totalChars; i++) {
+                    sb.append(separator);
+                }
+                result.add(sb.toString());
+            }
+            sb.append(words[currWordIndex]);
+            if (totalChars + words[currWordIndex].length() + 1 + words[currWordIndex + 1].length() < maxWidth)
+                sb.append(separator);
+            line.add(sb.toString());
+            sb = new StringBuilder();
+            totalChars += words[currWordIndex].length() + 1;
+            currWordIndex++;
         }
         return result;
     }
+
     public static void main(String[] args) {
         TextJustif tj = new TextJustif();
         String[] words = {"This", "is", "an", "example", "of", "text", "justification"};
         int maxWidth = 16;
-        for (String s : tj.fullJustify(words, 16)) {
+        for (String s : tj.methodTwo(words, 16)) {
             System.out.printf("\"%s\"\n", s);
         }
         words = new String[]{"What", "must", "be", "acknowledgment", "shall", "be"};
-        for (String s : tj.fullJustify(words, 16)) {
+        for (String s : tj.methodTwo(words, 16)) {
             System.out.printf("\"%s\"\n", s);
         }
         words = new String[]{"Science", "is", "what", "we", "understand", "well", "enough", "to",
                 "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"};
-        for (String s : tj.fullJustify(words, 20)) {
+        for (String s : tj.methodTwo(words, 20)) {
             System.out.printf("\"%s\"\n", s);
         }
     }
