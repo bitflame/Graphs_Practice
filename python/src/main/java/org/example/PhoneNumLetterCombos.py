902dcb1d2ba109889f8aae8f12cf8864e2c9b93e
shahin <shahinnumone@outlook.com>
2026-02-12 18:28:02 -0500

    The ruler exercise seems to work

--- python/src/main/java/org/example/PhoneNumLetterCombos.py
+++ python/src/main/java/org/example/PhoneNumLetterCombos.py
@@ -1,18 +1,30 @@
+from typing import List
+
+
 class Solution:
     def letter_combinations(self, digits: str) -> str:
         if len(digits) == 1: return digits
-        numbers = {1: '', 2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9:'wzyz',0:''}
-        print(numbers.get(3))
+        numbers = {1: '', 2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9: 'wzyz', 0: ''}
+        return ''.join(self.helper(digits, numbers, 0, []))
 
-
-    def helper(self, digits: str, numbers) -> str:
-        combinations = ''
-        for i, ch in enumerate(digits):
-            permutations = self.helper(digits[:i] + digits[i + 1:], numbers)
-            for permutation in permutations:
-                pass
+    def helper(self, digits: str, numbers, index, combinations) -> List:
+        if index == len(digits): return combinations
+        characters = numbers.get(int(digits[index]))
+        if len(combinations) == 0:
+            for character in characters:
+                combinations.append(character)
+        else:
+            new_combos = []
+            for character in characters:
+                for i in range(len(combinations)):
+                    segment = combinations[i] + character
+                    new_combos.append(segment)
+            combinations = new_combos
+        combinations = self.helper(digits, numbers, index + 1, combinations)
         return combinations
+
 
 digits = '23'
 s = Solution()
 print(s.letter_combinations(digits))
+
--- python/src/main/java/org/example/StringExercises.py
+++ python/src/main/java/org/example/StringExercises.py
@@ -1,6 +1,9 @@
 # ch four stuff write a function that checks a number to be binary, to run faster you can hard code the
 # ascii values of 1 and 0 so you don't have to look it up every time  also, you can use a variable for the
 # ascii of the digit so you don't have to call it twice
+from curses.ascii import isalpha
+
+
 def is_binary_number(number):
     if not number: return False
     for digit in number:
@@ -16,3 +19,20 @@ print(f'Test 4 - expected output: True, actual output: {is_binary_number('00')}'
 print(f'Test 5 - expected output: True, actual output: {is_binary_number('1')}')
 print(f'Test 6 - expected output: True, actual output: {is_binary_number('0')}')
 print(f'Test 7 - expected output: False, actual output: {is_binary_number('A')}')
+
+
+# another version of hte above method..
+def is_bin(number):
+    for num in number:
+        if not (int(num) == 1 or int(num) == 0) or isalpha(num):
+            return False
+    return True
+
+
+print(f'Test 1 - expected output: True, actual output: {is_bin('1100')}')
+print(f'Test 2 - expected output: False, actual output: {is_bin('1127')}')
+print(f'Test 3 - expected output: True, actual output: {is_bin('11')}')
+print(f'Test 4 - expected output: True, actual output: {is_bin('00')}')
+print(f'Test 5 - expected output: True, actual output: {is_bin('1')}')
+print(f'Test 6 - expected output: True, actual output: {is_bin('0')}')
+print(f'Test 7 - expected output: False, actual output: {is_bin('A')}')
