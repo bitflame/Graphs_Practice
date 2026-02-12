6a1d086cce292c8c6f5852d4e8cb8aa58ed84c16
shahin <shahinnumone@outlook.com>
2026-02-12 18:45:00 -0500

    The ruler exercise seems to work

--- python/src/main/java/org/example/StringExercises.py
+++ python/src/main/java/org/example/StringExercises.py
@@ -1,7 +1,6 @@
 # ch four stuff write a function that checks a number to be binary, to run faster you can hard code the
 # ascii values of 1 and 0 so you don't have to look it up every time  also, you can use a variable for the
 # ascii of the digit so you don't have to call it twice
-from curses.ascii import isalpha
 
 
 def is_binary_number(number):
@@ -24,7 +23,7 @@ print(f'Test 7 - expected output: False, actual output: {is_binary_number('A')}'
 # another version of hte above method..
 def is_bin(number):
     for num in number:
-        if not (int(num) == 1 or int(num) == 0) or isalpha(num):
+        if num.isalpha() or not (int(num) == 1 or int(num) == 0):
             return False
     return True
 
@@ -36,3 +35,19 @@ print(f'Test 4 - expected output: True, actual output: {is_bin('00')}')
 print(f'Test 5 - expected output: True, actual output: {is_bin('1')}')
 print(f'Test 6 - expected output: True, actual output: {is_bin('0')}')
 print(f'Test 7 - expected output: False, actual output: {is_bin('A')}')
+print('The next test will fail:')
+# print(f'Test 7 - expected output: Big Fat Failure, actual output: {is_bin(' ')}')
+
+def is_bin_optimized(number):
+    for i in number:
+        if i not in '10':
+            return False
+    return True
+print(f'Test 1 - expected output: True, actual output: {is_bin('1100')}')
+print(f'Test 2 - expected output: False, actual output: {is_bin('1127')}')
+print(f'Test 3 - expected output: True, actual output: {is_bin('11')}')
+print(f'Test 4 - expected output: True, actual output: {is_bin('00')}')
+print(f'Test 5 - expected output: True, actual output: {is_bin('1')}')
+print(f'Test 6 - expected output: True, actual output: {is_bin('0')}')
+print(f'Test 7 - expected output: False, actual output: {is_bin('A')}')
+print(f'Test 8 - expected output: False, actual output: {is_bin('!')}')
