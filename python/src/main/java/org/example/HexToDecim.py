08d7b871dc7e2fb4e360e75426733580465dacd4
shahin <shahinnumone@outlook.com>
2026-02-13 10:54:15 -0500

    The ruler exercise seems to work

--- python/src/main/java/org/example/HexToDecim.py
+++ python/src/main/java/org/example/HexToDecim.py
@@ -3,10 +3,17 @@ def hex_to_dec(number):
     result = 0
     for index, num in enumerate(reversed(number)):
         if num.isalpha():
-            result += map.get(num.tolower()) * 16 ** index
+            result += map.get(num.lower()) * 16 ** index
         else:
             result += int(num) * 16 ** index
     return result
 
 
-print(f'{hex_to_dec()}')
+print(f'expected output: 15, actual output: {hex_to_dec('f')}')
+print(f'expected output: 14, actual output: {hex_to_dec('e')}')
+print(f'expected output: 13, actual output: {hex_to_dec('d')}')
+print(f'expected output: 13, actual output: {hex_to_dec('D')}')
+print(f'expected output: 12, actual output: {hex_to_dec('c')}')
+print(f'expected output: 11, actual output: {hex_to_dec('b')}')
+print(f'expected output: 0, actual output: {hex_to_dec('0')}')
+print(f'expected output: 255, actual output: {hex_to_dec('ff')}')
