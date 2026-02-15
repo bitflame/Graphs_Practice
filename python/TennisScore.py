8a29cdb682be8461641938d782ef92b89cb1465e
shahin <shahinnumone@outlook.com>
2026-02-15 17:43:20 -0500

    Added pattern matching with rotation possibility

---
+++ python/TennisScore.py
@@ -0,0 +1,5 @@
+def tennis_score(score, player1_name, player2_name):
+    pass
+
+
+print(f'Test 1- 1:0, Micha, Tim. Expected output: Fifteen Love. Actual output: {tennis_score("1:0", "Micha", "Tim")}')
