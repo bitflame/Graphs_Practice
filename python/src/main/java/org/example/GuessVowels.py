e0ec4451c21e2b8b482c6f5b6ddde4e4e5a6f025
shahin <shahinnumone@outlook.com>
2026-02-16 15:52:37 -0500

    Added filled frame

---
+++ python/src/main/java/org/example/GuessVowels.py
@@ -0,0 +1,15 @@
+def translate_vowels(text, replacement):
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    result = []
+    for curr in text:
+        if curr.lower() in vowels:
+            result.append(replacement)
+        else:
+            result.append(curr)
+    return ''.join(result)
+
+
+print(f'expected output: g??d, actual output: {translate_vowels('guide', '?')}')
+print(f'expected output: l-wnm-w-r, actual output: {translate_vowels('lawnmower', '-')}')
+print(f'expected output: q z, actual output: {translate_vowels('quiz', '_')}')
+print(f'expected output: lwnmwr, actual output: {translate_vowels('lawnmower', '')}')
