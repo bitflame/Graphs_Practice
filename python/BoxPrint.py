3ccbdd8664e99794484189ca6bdeeb033c623714
shahin <shahinnumone@outlook.com>
2026-02-16 15:30:11 -0500

    Added filled frame

--- python/BoxPrint.py
+++ python/BoxPrint.py
@@ -1,33 +1,32 @@
 def print_box(width, height, fill_char):
-    x = 0
-    for y in range(height):
-        if x==0:
-            if y==0:
-                print('+', end=0)
-            if y==height-1:
-                print('+')
+    for row in range(height):
+        for col in range(width):
+            if row == 0:
+                if col == 0:
+                    print('+', end='')
+                if col == width - 1:
+                    print('+')
+                else:
+                    print('-', end='')
+            elif row == height - 1:
+                if col == 0:
+                    print('+', end='')
+                if col == width - 1:
+                    print('+')
+                else:
+                    print('-', end='')
             else:
-                print()
-        if x == width-1:
-            if y==0:
-                print('+', end=0)
-            if y==height-1:
-                print('+')
-        if (x == 0 and y == 0) or (x == 0 and y == height - 1) or (x == width - 1 and y == 0) or (
-                x == width - 1 and y == height - 1):
-            print('+')
-            x += 1
-        elif (x > 0 and x < width - 1 and y > 0) or (x > 0 and x < width - 1 and y < height - 1):
-            print('|', end='')
-            print('-' * width, end='')
-            print('|')
-            x += 1
-        else:
-            print('|', end='')
-            print(fill_char * width, end='')
-            print('|')
-            x += 1
+                if col == 0:
+                    print('|', end='')
+                if col == width - 1:
+                    print('|')
+                else:
+                    print(fill_char, end='')
 
-# need to use a nested loop
+
+print('     another run           ')
 print_box(8, 9, '*')
+print('     another run           ')
 print_box(8, 9, '@')
+print('     another run           ')
+print_box(8, 9, '$')
