742e1171ba22be0ad8f6d1f96e6b70d812b72bea
shahin <shahinnumone@outlook.com>
2026-03-03 18:37:07 -0500

    added a set to my find_common() and added book's method also.

--- python/BasicDataStructures/src/FindCommon.py
+++ python/BasicDataStructures/src/FindCommon.py
@@ -3,6 +3,7 @@ class FindCommon:
         if not values1 or not values2:
             raise ValueError("neither lists can be null")
         if len(vals_one) == 0 or len(values2) == 0: return []
+        seen = set()
         result = []
         m = len(values1)
         n = len(values2)
@@ -10,8 +11,10 @@ class FindCommon:
         values2.sort()
         l1, l2 = 0, 0
         while l1 < m and l2 < n:
-            if values1[l1] == values2[l2] and result.count(values1[l1])==0:
-                result.append(values1[l1])
+            if values1[l1] == values2[l2]:
+                if values1[l1] not in seen:
+                    result.append(values1[l1])
+                    seen.add(values1[l1])
                 l1 += 1
                 l2 += 1
             elif values1[l1] < values2[l2]:
@@ -41,3 +44,27 @@ print("expected answer [2,7], actual answer: ", f.find_common(vals_one, vals_two
 vals_one = [2,4,6,8]
 vals_two = [1,3,5,7,9]
 print("expected answer [], actual answer: ", f.find_common(vals_one, vals_two))
+
+# book's method..
+def find_common_book(values1, values2):
+    results = {}
+    populate_from_collection1(values1, results)
+    mark_if_also_in_second(values2, results)
+    return remove_all_just_in_first(results)
+def populate_from_collection1(values1, results):
+    for elem1 in values1:
+        results[elem1]=1
+def mark_if_also_in_second(values2, results):
+    for elem2 in values2:
+        if elem2 in results:
+            results[elem2]+=1
+def remove_all_just_in_first(results):
+    final_result = set()
+    for key, value in results.items():
+        if value >=2:
+            final_result.add(key)
+    return final_result
+
+
+def bestMethod(values1, values2):
+    return list(set(values1)&set(values2))
