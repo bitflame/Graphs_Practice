1e0a102e34c31ca8ed7479bf7b311117e6d1e367
shahin <shahinnumone@outlook.com>
2026-03-15 16:42:54 -0400

    Saving current mile stone

--- MyClass.py
+++ MyClass.py
@@ -14,52 +14,22 @@ hanoi(3, src, aux, dest)
 
 
 def get_dist(str_one, str_two):
-    len_one, len_two = len(str_one) - 1, len(str_two) - 1
-    return distance_helper(str_one, len_one, str_two, len_two, 0)
+    pos1, pos2 = len(str_one) - 1, len(str_two) - 1
+    return distance_helper(str_one, str_two, pos1, pos2)
 
 
-def distance_helper(str_one, len_one, str_two, len_two, curr_char_index):
-    modified_chars = 0
-    if curr_char_index > len_one and curr_char_index > len_two:
-        return min(len_one, len_two)
-    if curr_char_index > len_one and curr_char_index < len_two:
-        str_one = str_one + str_two[curr_char_index:]
-        return len_two - len_one
-    elif curr_char_index > len_two and curr_char_index < len_one:
-        str_two = str_two + str_one[curr_char_index:]
-        return len_one - len_two
-    elif str_one[curr_char_index] == str_two[curr_char_index]:
-        modified_chars = distance_helper(str_one, len_one, str_two, len_two, curr_char_index + 1)
+def distance_helper(str_one, str_two, pos1, pos2):
+    if pos1 < 0:
+        return pos2 + 1
+    if pos2 < 0:
+        return pos1 + 1
+    if str_one[pos1] == str_two[pos2]:
+        return distance_helper(str_one, str_two, pos1 - 1, pos2 - 1)
     else:
-        insert(str_one, curr_char_index, str_two[curr_char_index])
-        modified_chars = 1 + min(modified_chars,
-                                 distance_helper(str_one, len_one, str_two, len_two, curr_char_index))
-        remove(str_one, curr_char_index)
-        modified_chars = 1 + min(modified_chars,
-                                 distance_helper(str_one, len_one, str_two, len_two, curr_char_index))
-        replace(str_one, str_two, curr_char_index)
-        modified_chars = 1 + min(modified_chars,
-                                 distance_helper(str_one, len_one, str_two, len_two, curr_char_index))
-        curr_char_index += 1
-    return modified_chars
-
-
-def insert(target_str, index, additional_char):
-    print(f'modified {target_str}')
-    target_str = target_str[:index] + additional_char + target_str[index + 1:]
-    print(f'to: {target_str}')
-
-
-def remove(target_str, index):
-    print(f'modified {target_str}')
-    target_str = target_str[:index] + target_str[index + 1:]
-    print(f'to: {target_str}')
-
-
-def replace(target_str, source_str, index):
-    print(f'modified {target_str}')
-    target_str[index] = source_str[index]
-    print(f'to: {target_str}')
+        insert_in_first = distance_helper(str_one, str_two, pos1, pos2 - 1)
+        delete_in_first = distance_helper(str_one, str_two, pos1 - 1, pos2)
+        change = distance_helper(str_one, str_two, pos1 - 1, pos2 - 1)
+    return 1 + min(insert_in_first, delete_in_first, change)
 
 
 st_one = 'Micha'
@@ -68,5 +38,55 @@ print(f'Test 1 - expected answer: 2, actual answer: {get_dist(st_one, st_two)}')
 print(f'Test 2 - expected answer: 2, actual answer: {get_dist(st_two, st_one)}')
 st_one = 'rapple'
 st_two = 'tables'
-print(f'Test 3 - expected answer: 2, actual answer: {get_dist(st_two, st_one)}')
-print(f'Test 3 - expected answer: 2, actual answer: {get_dist(st_one, st_two)}')
+print(f'Test 3 - expected answer: 4, actual answer: {get_dist(st_two, st_one)}')
+print(f'Test 4 - expected answer: 4, actual answer: {get_dist(st_one, st_two)}')
+st_one = 'a'
+st_two = 'b'
+print(f'Test 5 - expected answer: 1, actual answer: {get_dist(st_two, st_one)}')
+print(f'Test 6 - expected answer: 1, actual answer: {get_dist(st_one, st_two)}')
+st_one = 'aa'
+st_two = 'bb'
+print(f'Test 7 - expected answer: 2, actual answer: {get_dist(st_two, st_one)}')
+print(f'Test 8 - expected answer: 2, actual answer: {get_dist(st_one, st_two)}')
+
+
+def get_dist_memo(str_one, str_two):
+    pos1, pos2 = len(str_one) - 1, len(str_two) - 1
+    return distance_helper_memo(str_one, str_two, pos1, pos2, {})
+
+
+def distance_helper_memo(str_one, str_two, pos1, pos2, values):
+    if pos1 < 0:
+        return pos2 + 1
+    if pos2 < 0:
+        return pos1 + 1
+    if (pos1, pos2) in values:
+        # print('cached value provided.')
+        return values[(pos1, pos2)]
+    if str_one[pos1] == str_two[pos2]:
+        result = distance_helper_memo(str_one, str_two, pos1 - 1, pos2 - 1, values)
+    else:
+        insert_in_first = distance_helper_memo(str_one, str_two, pos1, pos2 - 1, values)
+        delete_in_first = distance_helper_memo(str_one, str_two, pos1 - 1, pos2, values)
+        change = distance_helper_memo(str_one, str_two, pos1 - 1, pos2 - 1, values)
+        result = 1 + min(insert_in_first, delete_in_first, change)
+    values[(pos1, pos2)] = result
+    return result
+
+
+st_one = 'Micha'
+st_two = 'Michael'
+print(f'Test 1 - expected answer: 2, actual answer: {get_dist_memo(st_one, st_two)}')
+print(f'Test 2 - expected answer: 2, actual answer: {get_dist_memo(st_two, st_one)}')
+st_one = 'rapple'
+st_two = 'tables'
+print(f'Test 3 - expected answer: 4, actual answer: {get_dist_memo(st_two, st_one)}')
+print(f'Test 4 - expected answer: 4, actual answer: {get_dist_memo(st_one, st_two)}')
+st_one = 'a'
+st_two = 'b'
+print(f'Test 5 - expected answer: 1, actual answer: {get_dist_memo(st_two, st_one)}')
+print(f'Test 6 - expected answer: 1, actual answer: {get_dist_memo(st_one, st_two)}')
+st_one = 'aa'
+st_two = 'bb'
+print(f'Test 7 - expected answer: 2, actual answer: {get_dist_memo(st_two, st_one)}')
+print(f'Test 8 - expected answer: 2, actual answer: {get_dist_memo(st_one, st_two)}')
