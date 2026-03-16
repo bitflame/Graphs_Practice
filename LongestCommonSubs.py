fdef0a885098473404006581086fe9ab282346a9
shahin <shahinnumone@outlook.com>
2026-03-16 10:57:51 -0400

    Saving current milestone

--- LongestCommonSubs.py
+++ LongestCommonSubs.py
@@ -1,8 +1,4 @@
-import functools
 import time
-
-
-
 
 
 def lcs(str1, str2):
@@ -22,42 +18,7 @@ def lcs_helper(str1, str2, pos1, pos2):
 
 first_string = 'ABCE'
 second_string = 'ZACEF'
-print(f'Test 1 - expected answer: ACE, actual answer:  {lcs(first_string, second_string)}')
-
-
-def lcs_front(str1, str2):
-    return lcs_front_helper(str1, str2, 0, 0)
-
-def decorate_with_memo_shorter(str1, str2, pos1, pos2):
-    values = [[None for _ in range(len(str2))] for _ in range(len(str1))]
-
-    @functools.wraps(func)
-    def helper(pos1, pos2):
-        if values[pos1][pos2] == None:
-            values[pos1][pos2] = func
-        else:
-            return values[pos1][pos2]
-        return values
-
-    return func
-
-@decorate_with_memo_shorter
-def lcs_front_helper(str1, str2, pos1, pos2, values):
-    if pos1 >= len(str1) or pos2 >= len(str2):
-        return ""
-    if str1[pos1] == str2[pos2]:
-        return str1[pos1] + lcs_front_helper(str1, str2, pos1 + 1, pos2 + 1)
-    else:
-        lcs1 = lcs_front_helper(str1, str2, pos1, pos2 + 1)
-        lcs2 = lcs_front_helper(str1, str2, pos1 + 1, pos2)
-    return lcs1 if len(lcs1) > len(lcs2) else lcs2
-
-
-print(f'Test 1 - expected answer: ACE, actual answer:  {lcs_front(first_string, second_string)}')
-
-
-
-
+# print(f'Test 1 - expected answer: ACE, actual answer:  {lcs(first_string, second_string)}')
 
 '''@pytest.mark.parametrize("value1, value2, expected",
                          [("ABCE", "ZACED", "ACE"),
@@ -108,4 +69,43 @@ def main():
         print("lcs() took %.2f ms" % ((end - start) * 1000))
 
 
-main()
+def lcs_from_start(str1, str2):
+    values = [[None for _ in range(len(str2))] for _ in range(len(str1))]
+    return lcs_from_start_helper(str1, str2, 0, 0, values)
+
+
+def lcs_from_start_helper(str1, str2, pos1, pos2, values):
+    if pos1 >= len(str1) or pos2 >= len(str2):
+        return ""
+    if values[pos1][pos2] is not None:
+        return values[pos1][pos2]
+    if str1[pos1] == str2[pos2]:
+        return str1[pos1] + lcs_from_start_helper(str1, str2, pos1 + 1, pos2 + 1, values)
+    else:
+        lcs1 = lcs_from_start_helper(str1, str2, pos1, pos2 + 1, values)
+        lcs2 = lcs_from_start_helper(str1, str2, pos1 + 1, pos2, values)
+        result = lcs1 if len(lcs1) > len(lcs2) else lcs2
+        values[pos1][pos2] = result
+    return result
+
+
+def main_two():
+    # inputs_tuples = [["ABCMIXCHXAEL", "MICHAEL"],
+    #                  ["sunday-Morning", "saturday-Night-party"],
+    #                  ["sunday-morning-Wakeup", "saturday-Night"]]
+    inputs_tuples = [["ABCE", "ZACEF"],
+                     ["Micha", "Michael"],
+                     ["ABCXY", "XYACB"],
+                     ["ABCMIXCHXAEL", "MICHAEL"],
+                     ["sunday-Morning", "saturday-Night-party"],
+                     ["sunday-Morning-Wakeup", "saturday-Night"]]
+    for i, inputs in enumerate(inputs_tuples):
+        start = time.process_time()
+        result = lcs_from_start(inputs[0], inputs[1])
+        end = time.process_time()
+
+        print(f"Test {1 + i} - " + inputs[0] + " -> " + inputs[1] + " lcs:" + result)
+        print("lcs() took %.2f ms" % ((end - start) * 1000))
+
+
+main_two()
