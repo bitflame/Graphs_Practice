1eeca26f85ac4345b5775b9c016a66526b071db9
shahin <shahinnumone@outlook.com>
2026-03-17 15:52:44 -0400

    Minor update

--- NewEffort.py
+++ NewEffort.py
@@ -12,7 +12,7 @@ def puzzle_solver_helper(board, start_row, start_col):
         return True
     row = start_row
     col = start_col
-    while board[row][col] != "0":
+    while board[row][col] != 0:
         col += 1
         if col > 8:
             row += 1
@@ -23,26 +23,56 @@ def puzzle_solver_helper(board, start_row, start_col):
     for num in range(1, 10):
         board[row][col] = num
         if is_valid_position(board):
+            if col < 8:
+                solved = puzzle_solver_helper(board, row, col + 1)
+            else:
+                solved = puzzle_solver_helper(board, row + 1, 0)
+            if not solved:
+                board[row][col] = 0
+            else:
+                return True
+        else:
+            board[row][col] = 0
+    return False
 
 
 def is_valid_position(board):
-    return check_horizontally(board) and check_vertically(board) and check_box(board)
+    return check_horizontally(board) and check_vertically(board) and check_boxes(board)
+
+
+def check_boxes(board):
+    for y_box in range(3):
+        for x_box in range(3):
+            box_values = collect_box_values(board, y_box, x_box)
+            if not all_desired_numbers(box_values):
+                return False
+    return True
+
+
+def collect_box_values(board, y, x):
+    box_values = []
+    for row in range(3):
+        for col in range(3):
+            box_values.append(board[y * 3 + row][x * 3 + col])
+    return box_values
 
 
 def remove_all_occurrences(values, val):
     return [value for value in values if value != val]
 
 
+# checks the board one column at a time for valid content
 def check_vertically(board):
     for x in range(9):
-        col_values =[board[row][x] for row in range(9)]
+        col_values = [board[row][x] for row in range(9)]
         if not all_desired_numbers(col_values):
             return False
     return True
 
+
 # checks every row for having desired values i.e. it checks every col for every row every time it is called
 def check_horizontally(values):
-    for row in values:
+    for row in range(9):
         row_values = [values[row][x] for x in range(9)]
         if not all_desired_numbers(row_values):
             return False
@@ -56,6 +86,19 @@ def all_desired_numbers(all_collected_values):
     value_set = set(relevant_values)
     if len(value_set) != len(relevant_values): return False
     return value_set.issubset([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+some_board = [[1, 2, 0, 4, 5, 0, 7, 8, 9],
+              [0, 5, 6, 7, 0, 9, 0, 2, 3],
+              [7, 8, 0, 1, 2, 3, 4, 5, 6],
+              [2, 1, 4, 0, 6, 0, 8, 0, 7],
+              [3, 6, 0, 8, 9, 7, 2, 1, 4],
+              [0, 9, 7, 0, 1, 4, 3, 6, 0],
+              [5, 3, 1, 6, 0, 2, 9, 0, 8],
+              [6, 0, 2, 9, 7, 8, 5, 3, 1],
+              [9, 7, 0, 0, 3, 1, 6, 4, 2]]
+print("-----Here is the result of current code:")
+print(puzzle_solver(some_board))
 
 
 # 0 represents a blank spot
@@ -75,6 +118,20 @@ def print_board(board):
             print(board[i][j], ' ', end='')
         print()
     return None
-
-
+print("Here is the board after solving it: ")
+print_board(some_board)
 print_board(my_board)
+
+
+def get_row(board):
+    print('Here are the rows one at a time ')
+    for row in range(9):
+        current_row = [board[row][col] for col in range(9)]
+
+
+get_row(my_board)
+
+
+def get_column(board):
+    for col in range(9):
+        current_column = [board[row][col] for row in range(9)]
