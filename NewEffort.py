5193a671cb119e2ac7443a3fe18d992c6d7f3bc9
shahin <shahinnumone@outlook.com>
2026-03-17 19:21:37 -0400

    Minor update

--- NewEffort.py
+++ NewEffort.py
@@ -135,3 +135,26 @@ get_row(my_board)
 def get_column(board):
     for col in range(9):
         current_column = [board[row][col] for row in range(9)]
+
+
+test_board = [[ 1, 2, 3, 4, 5, 6, 7, 8, 9],
+              [10,11,12,13,14,15,16,17,18],
+              [19,20,21,22,23,24,25,26,27],
+              [28,29,30,31,32,32,33,34,35],
+              [36,37,38,39,40,41,42,43,44],
+              [45,46,47,48,49,50,51,52,53],
+              [54,55,56,57,58,59,60,61,62],
+              [63,64,65,66,67,68,69,70,71],
+              [72,73,74,75,76,77,78,79,80]]
+
+
+def get_box_content(board, row, col):
+    content = []
+    adjusted_row = row // 3 * 3
+    adjusted_col = col // 3 * 3
+    for y in range(3):
+        for x in range(3):
+            content.append(board[adjusted_row+y][adjusted_col+x])
+    print(content)
+
+get_box_content(test_board, 3,4)
