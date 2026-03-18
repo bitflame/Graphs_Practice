ed46bf1257b192b046443c1405f6f41d0ee9a839
shahin <shahinnumone@outlook.com>
2026-03-18 19:10:25 -0400

    Minor update

--- RecursivePalindromeFinder.py
+++ RecursivePalindromeFinder.py
@@ -1,22 +1,77 @@
-def finder(variable_string):
-    return helper(variable_string.lower(), 0, 0, [], [])
 
 
-def helper(current_string, start, end, current_pal, results):
-    if start < 0 or end > len(current_string) - 1:
-        return results
-    if current_string[start] == current_string[end]:
-        current_pal.append(current_string[start])
-        if end > start:
-            current_pal.append(current_string[end])
-        helper(current_string, start - 1, end + 1, current_pal, results)
-    elif len(current_pal) > 1:
-        results.append(current_pal)
-        return results
-    results = helper(current_string, start + 1, end + 1, [], results)
+
+def all_palindrom_parts(input):
+    results = set()
+    all_palindrome_parts_rec(input, 0, len(input)-1, results)
     return results
+def all_palindrome_parts_rec(input, left, right, results):
+    # recursive termination
+    if left >= right:
+        return
+    #1) check if the whole string is a palindrome
+    complete_is_palindrome = is_palindrome_rec_range(input, left, right)
+    if complete_is_palindrome:
+        new_candidate = input[left:right+1]
+        results.add(new_candidate)
+    for i in range(left+1, right):
+        left_part_is_palindrome = is_palindrome_rec_range(input,i, right)
+        if left_part_is_palindrome:
+            new_candidate = input[i:right+1]
+            results.add(new_candidate)
+    for i in range(right-1, left, -1):
+        right_part_is_palindrome = is_palindrome_rec_range(input, left, i)
+        if right_part_is_palindrome:
+            new_candidate = input[left: i+1]
+            results.add(new_candidate)
+    # recursive descent
+    all_palindrome_parts_rec(input, left+1, right-1, results)
 
+def is_palindrome_rec_range(input, left, right):
+    if left >= right:
+        return True
+    if input[left]==input[right]:
+        # recursive descent
+        return is_palindrome_rec_range(input, left+1, right-1)
+    return False
 
-print(finder("BCDEDCB"))
-print(finder("ABALOTTOLL"))
-print(finder("racecar"))
+# the following optimized version calls itself for the shortened version from left and right, instead of reduing
+# the input from both sides and having to iterate through shorter versions of each side repeatedly, so there are
+# no loops in this one.
+def all_palindrome_parts_rec_optimized(input):
+    results = set()
+    all_palindrom_parts_rec_optimized_(input, 0,len(input)-1, results)
+    return results
+def all_palindrom_parts_rec_optimized_(input, left, right, results):
+    if left>= right:
+        return
+    if is_palindrome_rec_range(input, left, right):
+        results.add(input[left:right+1])
+    # recursive descent
+    all_palindrom_parts_rec_optimized_(input, left+1, right, results)
+    all_palindrom_parts_rec_optimized_(input, left, right-1, results)
+
+# print(all_palindrom_parts("BCDEDCB"))
+# print(all_palindrom_parts("ABALOTTOLL"))
+# print(all_palindrom_parts("racecar"))
+
+# print(all_palindrome_parts_rec_optimized("BCDEDCB"))
+# print(all_palindrome_parts_rec_optimized("ABALOTTOLL"))
+# print(all_palindrome_parts_rec_optimized("racecar"))
+
+# yet a more readable version...
+def all_pal_parts_rec_opt(input):
+    results = set()
+    all_palindrom_parts_v3(input, results)
+    return results
+def all_palindrom_parts_v3(input, results):
+    if len(input) < 2:
+        return
+    if is_palindrome_rec_range(input, 0, len(input)-1):
+        results.add(input)
+    all_palindrom_parts_v3(input[1:], results)
+    all_palindrom_parts_v3(input[0:len(input)-1],results)
+
+print(all_pal_parts_rec_opt("BCDEDCB"))
+print(all_pal_parts_rec_opt("ABALOTTOLL"))
+print(all_pal_parts_rec_opt("racecar"))
