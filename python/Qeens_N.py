e321d9db11ed95e90c16baf0e1bb4bf08c0dcdb0
shahin <shahinnumone@outlook.com>
2026-03-19 16:29:35 -0400

    Minor update

--- python/Qeens_N.py
+++ python/Qeens_N.py
@@ -1,26 +1,29 @@
+from platform import android_ver
+
+
 def left_diagnol_clear(grid, current_row, current_col):
     while current_row > -1 and current_col > -1:
         current_row -= 1
         current_col -= 1
-        if grid[current_row][current_col] == 'Q':
+        if current_col > -1 and grid[current_row][current_col] == 'Q':
             return False
     return True
 
 
 def right_diagnol_clear(grid, current_row, current_col):
     while current_row > -1 and current_col < len(grid):
-        if grid[current_row][current_col] == 'Q':
-            return False
         current_row -= 1
         current_col += 1
+        if current_col < len(grid) and grid[current_row][current_col] == 'Q':
+            return False
     return True
 
 
 def vertical_is_clear(grid, current_row, current_col):
     while current_row > -1:
+        current_row -= 1
         if grid[current_row][current_col] == 'Q':
             return False
-        current_row -= 1
     return True
 
 
@@ -30,16 +33,20 @@ def start_placing_queens(grid_size, number_of_queens):
 
 
 def helper_method_to_place_queens(grid, number_of_queens, current_row, current_col):
+    solved = False
     if number_of_queens == 0:
-        return grid
+        return True
     else:
-        grid[current_row][current_col] = 'Q'
-        if (vertical_is_clear(grid, current_row, current_col)
-                and left_diagnol_clear(grid, current_row,current_col)
-                and right_diagnol_clear(grid, current_row, current_col)):
-            helper_method_to_place_queens(grid, number_of_queens - 1, current_row + 1, current_col)
-        else:
-            helper_method_to_place_queens(grid, number_of_queens, current_row, current_col + 1)
+        for i in range(0, len(grid[0])):
+            if (vertical_is_clear(grid, current_row, i) and
+                    left_diagnol_clear(grid, current_row, i) and right_diagnol_clear(
+                        grid, current_row, i)):
+                grid[current_row][i] = 'Q'
+                current_col = i
+                solved = helper_method_to_place_queens(grid, number_of_queens - 1, current_row + 1,
+                                              current_col + 1)
+                if not solved : grid[current_row][current_col] = ' '
+        return solved
 
 
-start_placing_queens(4, 4)
+print(start_placing_queens(4, 4))
